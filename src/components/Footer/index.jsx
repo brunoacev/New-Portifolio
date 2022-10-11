c1426c991f056cdc91220f3e45b8f0ec5153ab78
Bruno Azevedo <93809602+brunoacev@users.noreply.github.com>
2022-10-11 15:18:45 -0300

    Feat: Update Texto Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,7 +4,9 @@ import { Container } from "./styles";
 const Footer = () => {
   return (
     <Container className="footer">
-      <h1>Obrigado pela visita</h1>
+      
+      <p>Aquele que deslocou a montanha é o que comecou a remover as pequenas pedras. - Provérbio Chinês</p>
+      <h1>Obrigado pela visita!</h1>
     </Container>
   );
 };
